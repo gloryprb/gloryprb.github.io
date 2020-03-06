b726f763f823cf7647777d3aa46002b652e18b94
gloryprb <gloryypurbaa@gmail.com>
2020-03-06 16:43:35 +0700

    deploy

---
+++ precache-manifest.a6dc0fae0bac70f568c0db564731194f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a33a23d88fc04749e4168d81468323",
+    "url": "/index.html"
+  },
+  {
+    "revision": "486c5f988b853ad65cf0",
+    "url": "/static/css/main.ebb4725c.chunk.css"
+  },
+  {
+    "revision": "5a5aeceed8e9f9a4eb56",
+    "url": "/static/js/2.faef7496.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.faef7496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486c5f988b853ad65cf0",
+    "url": "/static/js/main.3696ff96.chunk.js"
+  },
+  {
+    "revision": "2fc8e7369f6406e5984d",
+    "url": "/static/js/runtime-main.447fd809.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
